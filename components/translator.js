19e1f3064c54abe372eddabf12dcd68536baffe5
delm83 <delm83@googlemail.com>
2024-02-21 14:18:59 +0000

    beginning translator

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,21 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    convertToBritish(text){
+      let currentWord='';
+    let words= text.split(/[\s]+/);
+        let translation = '';
+       for (let x in words)
+  if (Object.keys(americanToBritishSpelling).includes(words[x])){
+translation+=americanToBritishSpelling[words[x]]+' ';
+  }else translation+=words[x]+' ';
+        return translation;
+      }
 
+      convertToAmerican(text){
+        let words= text.split(/[\s]+/);
+        return words;
+      }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,18 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
-    });
+      let translation = '';
+      let text = req.body.text;
+      let locale = req.body.locale;
+      if(!text||!locale) return res.json({error: 'Required field(s) missing'});
+      if(text=='') return res.json({error: 'No text to translate'});
+      if(locale!='american-to-british'&&locale!='british-to-american') return res.json({error: 'Invalid value for locale field'});
+      if(locale=='american-to-british') {
+        translation = translator.convertToBritish(text);
+        return res.json({text: text, translation: translation});   }   
+      if(locale == 'british-to-american') {
+        translation = translator.convertToAmerican(text);
+        return res.json({text: text, translation: translation});  
+      }
+      });
 };
