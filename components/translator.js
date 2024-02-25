7dbba4d07f34eefbdc502c0a5b3b229a103ea971
delm83 <delm83@googlemail.com>
2024-02-25 13:22:44 +0000

    completed british to american method

--- components/translator.js
+++ components/translator.js
@@ -5,31 +5,50 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
     convertToBritish(text){
-      let regex = /\d\d\:\d\d/
+      let regex = /\d\d:\d\d/
       let match = text.match(regex);
       if(match){text = text.replace(match[0], '<span class="highlight">'+match[0].split(':').join('.')+'</span>');}
        for (let x in americanToBritishSpelling){
        regex = new RegExp(x, 'gi');
        match= text.match(regex);
-       if(match==x){text = text.replace(match, '<span class="highlight">'+americanToBritishSpelling[x]+'</span>')}
+       if(match){text = text.replace(match, '<span class="highlight">'+americanToBritishSpelling[x]+'</span>')}
        }
        for (let x in americanOnly){
             regex = new RegExp('\\b'+x+'\\b', 'gi');
             match= text.match(regex);
-            if(match==x){text = text.replace(match, '<span class="highlight">'+americanOnly[x]+'</span>')}
+            if(match){text = text.replace(match, '<span class="highlight">'+americanOnly[x]+'</span>')}
             }
             for (let x in americanToBritishTitles){
-                  regex = new RegExp(x, 'gi');
+                  regex = new RegExp(americanToBritishTitles[x]+'\.', 'gi');
                   match= text.match(regex);
-                  if(match==x){text = text.replace(match, '<span class="highlight">'+americanToBritishTitles[x]+'</span>')}
+                  if(match==x){
+                    text = text.replace(match, '<span class="highlight">'+americanToBritishTitles[x]+'</span>')}
                   }
       return text.trim();
       }
       
 
       convertToAmerican(text){
-        let words= text.split(/[\s]+/);
-        return words;
+        let regex = /\d\d:\d\d/
+        let match = text.match(regex);
+        if(match){text = text.replace(match[0], '<span class="highlight">'+match[0].split('.').join(':')+'</span>');}
+         for (let x in americanToBritishSpelling){
+         regex = new RegExp(americanToBritishSpelling[x], 'gi');
+         match= text.match(regex);
+         if(match){text = text.replace(match, '<span class="highlight">'+x+'</span>')}
+         }
+         for (let x in britishOnly){
+              regex = new RegExp('\\b'+x+'\\b', 'gi');
+              match= text.match(regex);
+              if(match){text = text.replace(match, '<span class="highlight">'+britishOnly[x]+'</span>')}
+              }
+              for (let x in americanToBritishTitles){
+                    regex = new RegExp('\\b'+americanToBritishTitles[x]+'\\b', 'gi');
+                    match= text.match(regex);
+                    if(match==americanToBritishTitles[x]){
+                      text = text.replace(match, '<span class="highlight">'+x+'</span>')}
+                    }
+        return text.trim();
       }
 }
 
